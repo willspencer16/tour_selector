b4c7af9316bb2cb0af7dae35cd55faa49c5322c7
Will Spencer <willspencer16@gmail.com>
2021-06-22 17:11:10 +0100

    App fetches data from api and displays it to console.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading,setLoading] = useState(true);
   const [tours,setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true)
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+
+  useEffect(()=>{
+    fetchTours();
+  }, []);
+
   if (loading){
     return (
       <main>
@@ -13,7 +25,11 @@ function App() {
       </main>
     );
   }
-  return <h2>Tours Project Setup</h2>
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App
