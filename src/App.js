c309b209e7bccb02967935f0c507908e59d09869
Will Spencer <willspencer16@gmail.com>
2021-06-23 11:15:13 +0100

    Added refresh button that appears if a user selects Not Interested to all tours

--- src/App.js
+++ src/App.js
@@ -37,6 +37,16 @@ function App() {
       </main>
     );
   }
+  if(tours.length == 0){
+    return <main>
+      <div className='title'>
+        <h2>No Tours Left</h2>
+        <button className='btn' onClick={fetchTours}>
+          Refresh
+        </button>
+      </div>
+    </main>
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
